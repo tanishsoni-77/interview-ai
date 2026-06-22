af763a0e6c14042a5f8d1af9964c8fdcc91394e1
tanishsoni-77 <sonitanish1305@gmail.com>
2026-06-22 20:11:34 +0530

    Initial Interview AI backend setup

---
+++ server.js
@@ -0,0 +1,10 @@
+require("dotenv").config()
+const app = require("./src/app")
+const connectTODB = require("./src/config/database")
+
+connectTODB()
+
+app.listen(3000,() => {
+    console.log("server is running on port 3000")
+})
+
