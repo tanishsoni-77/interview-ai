794a392e3d61bd73e82307968dce87fe8e0008ec
tanishsoni-77 <sonitanish1305@gmail.com>
2026-06-29 16:12:41 +0530

    Persist user session using getMe API on app load

--- Frontend/src/features/auth/auth.context.jsx
+++ Frontend/src/features/auth/auth.context.jsx
@@ -1,11 +1,15 @@
 import { createContext,useState } from "react";
+import { getMe } from "./services/auth.api";
 
 export const AuthContext =  createContext()
 
 export const AuthProvider = ({ children }) =>{
 
     const [user, setUser] = useState(null)
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(true)
+
+   
+
 
 
     return(
--- Frontend/src/features/auth/hooks/useAuth.js
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext,useEffect } from "react";
 import { AuthContext } from "../auth.context";
 import { register,login,logout,getMe } from "../services/auth.api";
 
@@ -46,5 +46,18 @@ export const useAuth = () =>{
         setLoading(false)
         }
     }
+     useEffect(() =>{
+        const getAndSetUser = async() =>{
+            const data = await getMe()
+            setUser(data.user)
+            setLoading(false)
+        }
+        getAndSetUser()
+
+    },[])
+
+
+
+
     return {user,loading,handleRegister,handleLogin,handleLogout}
 }
