e817efd6e8e2636234e7b4a53b7d5f8375d2de34
tanishsoni-77 <sonitanish1305@gmail.com>
2026-07-02 18:28:04 +0530

    Fix AI interview report schema generation

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,12 @@
 require("dotenv").config()
 const app = require("./src/app")
 const connectTODB = require("./src/config/database")
+const {resume,selfDescription, jobDescription} = require ("./src/services/temp")
+const generateInterviewReport = require ("./src/services/ai.service")
+
 
 connectTODB()
+generateInterviewReport ({resume,selfDescription, jobDescription})
 
 app.listen(3000,() => {
     console.log("server is running on port 3000")
