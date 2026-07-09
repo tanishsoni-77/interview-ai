12f761e57ec438695fafb687abc5198bffc59d24
tanishsoni-77 <sonitanish1305@gmail.com>
2026-07-09 15:53:26 +0530

    feat: add interview report API service functions

---
+++ Frontend/src/features/interview/services/interview.api.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const  api = axios.create({
+    baseURL: "http://localhost:3000",
+    withCredentials :true
+})
+
+/**
+ * @description generate new interview report on the basis of user 
+ * selfdescription , resume pdf and job description
+ */
+
+export const generateInterviewReport = async ({jobDescription, selfDescription, resumeFile}) => {
+    const formData = new FormData();
+    formData.append("jobDescription", jobDescription);
+    formData.append("selfDescription", selfDescription);
+    formData.append("resume", resumeFile);
+
+   const reponse = await api.post("/api/interview/", formData, {
+        headers: {
+            "Content-Type": "multipart/form-data"
+        }
+    })
+    return reponse.data
+}
+
+
+/**
+ * @description get interview report on the basis of interviewId
+ */
+
+export const getInterviewReportById =async (interviewId) => {
+   const response =  await api.get(`/api/interview/report/${interviewId}`)
+   return response.data
+}
+/**
+ * @description get all interview reports of the logged in user
+ */ 
+
+export const getAllInterviewReports = async () => {
+    const response = await api.get("/api/interview/")
+    return response.data
+}
