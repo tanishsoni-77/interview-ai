ba87b87f9848c4cd140ec52f629ce68cbb512064
tanishsoni-77 <sonitanish1305@gmail.com>
2026-07-29 16:39:13 +0530

    fix: resolve protected route import case issue

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from "react-router";
 import Login from "./features/auth/pages/Login";
 import Register from "./features/auth/pages/Register";
 import Home from "./features/interview/pages/Home";
-import Protected from "./features/auth/components/protected";
+import Protected from "./features/auth/components/Protected";
 import Interview from "./features/interview/pages/Interview";
 export const router =  createBrowserRouter([
     {
