98b43b946ed5f16e65091de9ac0d60c6d52c7f4f
tanishsoni-77 <sonitanish1305@gmail.com>
2026-07-30 21:06:58 +0530

    fix error

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,12 @@
 require("dotenv").config()
 const app = require("./src/app")
 const connectTODB = require("./src/config/database")
+const PORT = process.env.PORT || 3000;
 
+connectTODB();
 
-connectTODB()
 app.set("trust proxy", 1);
 
-app.listen(3000,() => {
-    console.log("server is running on port 3000")
-})
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
